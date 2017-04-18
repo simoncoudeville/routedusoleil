266d3d53a483f7099805b9a34ce0e282e6317032
Simon Coudeville <simon.coudeville@wijs.be>
2017-04-18 07:16:57 +0200

    added autoplay

--- js/theme.js
+++ js/theme.js
@@ -31,6 +31,7 @@ var chopstick =
         // initialize flickity
 
         $('.js-intro-carousel').flickity({
+            autoPlay: true,
             arrowShape: 'M83,45.9v8.1H32.8l23,23.2L50,83L17,50l33-33l5.8,5.8l-23,23.2H83z'
         });
 
